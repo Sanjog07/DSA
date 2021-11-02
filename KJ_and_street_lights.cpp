e79edaf4f613ca40a665ec97c6973546c0a09b89
Sanjog07 <sanjogkukreja12@gmail.com>
2021-11-02 13:20:14 +0530

    improved the time complexity

--- KJ_and_street_lights.cpp
+++ KJ_and_street_lights.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//  SOLUTION VIDEO : https://www.youtube.com/watch?v=uAAhD9rd3N8
+
+// in this problem we're using Scanline Algo
+
 int main(){
 	int n,p;
   cin>>n>>p;
@@ -14,23 +18,46 @@ int main(){
     int ry = (x+r);
 
     int st = (rx < 0) ? 0 : rx;
-    int end = (ry > p) ? p : ry;
+    v[st] += 1; // i.e ye wala step scanline algo ka part hai
 
-    for(int j=st; j<=end; j++){
-      if(v[j] == 1 || v[j] == -1){
-        v[j] = -1;
-      }else{
-        v[j] = 1;
-      }
+    // if((ry+1) <= p){
+    //   v[ry+1] -= 1; // i.e ye wala step scanline algo ka part hai
+    // }
+
+
+    // line 28-36 ki jagha sirf line 23-25 bhi likh skte hai, that will also work but usse thodi clarity nhi aa rhi thi
+    int end;
+    if(ry <= p-1){
+        end = ry;
+    }else{
+        end = p-1;
     }
+      
+    v[end+1] -= 1; // i.e ye wala step scanline algo ka part hai
+
+    // for(int j=st; j<=end; j++){
+    //   if(v[j] == 1 || v[j] == -1){
+    //     v[j] = -1;
+    //   }else{
+    //     v[j] = 1;
+    //   }
+    // }
 
   }
 
   int cnt = 0;
   int ans = INT_MIN;
 
+  vector<int> preSum(p+1);
+  preSum[0] = v[0];
+  int sum = v[0];
+  for(int i=1; i<=p; i++){
+    sum += v[i];
+    preSum[i] = sum;
+  }
+
   for(int i=0; i<=p; i++){
-    if(v[i] == 0 || v[i] == -1){
+    if(preSum[i] == 0 || preSum[i] > 1){
       cnt++;
     }else{
       ans = max(ans , cnt);
